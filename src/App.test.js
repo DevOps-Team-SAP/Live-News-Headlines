c8eebb0b8d72db62cb0705640defa8767c402f10
Mohithraj B J <mohithraj.b.j@sap.com>
2021-06-02 20:17:58 +0530

    Update Test File

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import NavBar from "./components/Navbar";
 import { render } from "@testing-library/react";
 
-test("renders Navbar", () => {
+test("Test Renders Navbar", () => {
   const { getByText } = render(<NavBar />);
   const h1 = getByText(/News Daily - Read news from anywhere/);
   expect(h1).toHaveTextContent("News Daily - Read news from anywhere");
